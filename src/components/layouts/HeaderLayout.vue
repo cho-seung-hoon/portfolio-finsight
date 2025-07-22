bebe2f2689d3e565d0e91436c1d7ce0ee161372f
crolvlee <leecrolv@gmail.com>
2025-07-22 16:09:01 +0900

    :bug: fix : HeaderLayout 추가

---
+++ src/components/layouts/HeaderLayout.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="layout-container">
+    <div class="header">
+      <Header />
+    </div>
+    <div class="content-container">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import Header from './Header.vue';
+</script>
+
+<style scoped>
+.layout-container {
+  display: flex;
+  background-color: var(--off-white); /* 본문 배경색 */
+  flex-direction: column;
+  height: 100vh;
+  overflow-y: auto;
+}
+
+.header {
+  height: 56px;
+  flex-shrink: 0;
+}
+
+.content-container {
+  flex: 1;
+  position: relative;
+  padding: 0 20px;
+}
+
+/* 스크롤바 커스터마이징 */
+.layout-container::-webkit-scrollbar {
+  width: 8px;
+}
+
+.layout-container::-webkit-scrollbar-track {
+  background-color: var(--off-white); /* 본문 배경색 */
+}
+
+.layout-container::-webkit-scrollbar-thumb {
+  background-color: var(--main03);
+  border-radius: 6px;
+  border: 2px solid var(--off-white);
+}
+
+.layout-container::-webkit-scrollbar-thumb:hover {
+  background-color: var(--sub01);
+}
+</style>
