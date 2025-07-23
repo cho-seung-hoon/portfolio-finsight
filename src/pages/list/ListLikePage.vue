edb9f38136eaafc38b99ac1f502d7a0904d89503
crolvlee <leecrolv@gmail.com>
2025-07-23 09:31:37 +0900

    :sparkles: feat : 찜한 목록 페이지 구성

--- src/pages/list/ListLikePage.vue
+++ src/pages/list/ListLikePage.vue
@@ -1,12 +1,34 @@
 <template>
-  <div>찜한 목록</div>
+  <section class="list-like-page-tab">
+    <button
+      v-for="tab in tabs"
+      :key="tab.value"
+      :class="['tab-btn', { active: selected === tab.value }]"
+      @click="selectTab(tab.value)">
+      {{ tab.label }}
+    </button>
+  </section>
+  <section class="list-like-page-content">
+    <DepositsItem></DepositsItem>
+  </section>
 </template>
 
 <script setup>
-import { onMounted } from 'vue';
+import { ref, onMounted } from 'vue';
 import { useHeaderStore } from '@/stores/header';
+import DepositsItem from '@/components/list/DepositsItem.vue';
 
 const headerStore = useHeaderStore();
+const tabs = [
+  { label: '예금', value: 'deposit' },
+  { label: '펀드', value: 'fund' },
+  { label: 'ETF', value: 'etf' }
+];
+const selected = ref('deposit');
+
+function selectTab(tab) {
+  selected.value = tab;
+}
 
 onMounted(() => {
   headerStore.setHeader({
@@ -17,4 +39,30 @@ onMounted(() => {
 });
 </script>
 
-<style scoped></style>
+<style scoped>
+.list-like-page-tab {
+  display: flex;
+  gap: 12px;
+  padding: 12px 0;
+  justify-content: center;
+}
+
+.tab-btn {
+  padding: 8px 12px;
+  border: none;
+  border-radius: 8px;
+  background: var(--main04);
+  color: var(--main02);
+  font-size: var(--font-size-ms);
+  font-weight: var(--font-weight-semi-bold);
+  transition:
+    background 0.15s,
+    color 0.15s;
+  cursor: pointer;
+}
+
+.tab-btn.active {
+  background: var(--main01);
+  color: var(--white);
+}
+</style>
