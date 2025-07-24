033854879953c2808c92bdd9ac86e6cf7d54ce4f
crolvlee <leecrolv@gmail.com>
2025-07-24 11:31:23 +0900

    :bug: fix : 찜한 목록에 임시 데이터 넣음

--- src/pages/list/ListLikePage.vue
+++ src/pages/list/ListLikePage.vue
@@ -9,7 +9,10 @@
     </button>
   </section>
   <section class="list-like-page-content">
-    <DepositItem></DepositItem>
+    <DepositItem
+      v-for="item in deposits"
+      :key="item.product_code"
+      :item="item" />
   </section>
 </template>
 
@@ -19,12 +22,31 @@ import { useHeaderStore } from '@/stores/header';
 import DepositItem from '@/components/list/DepositItem.vue';
 
 const headerStore = useHeaderStore();
+
 const tabs = [
   { label: '예금', value: 'deposit' },
   { label: '펀드', value: 'fund' },
   { label: 'ETF', value: 'etf' }
 ];
+
 const selected = ref('deposit');
+
+const deposits = [
+  {
+    product_code: 'SH123',
+    product_name: '시그니처 정기예금',
+    company_name: '신한은행',
+    intr_rate: 1.2,
+    intr_rate2: 2.8
+  },
+  {
+    product_code: 'KB123',
+    product_name: '플러스 정기예금',
+    company_name: '국민은행',
+    intr_rate: 1.5,
+    intr_rate2: 3.0
+  }
+];
 
 function selectTab(tab) {
   selected.value = tab;
