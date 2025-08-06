2c0169f3826d8845fba8540ac84dccd503cabb48
Jinwook Lee <gfdsa9497@naver.com>
2025-08-06 14:19:11 +0900

    :sparkles: feat : newsPublisher 추가

--- src/main/java/com/finsight/backend/adapter/ProductAdapter.java
+++ src/main/java/com/finsight/backend/adapter/ProductAdapter.java
@@ -1,26 +1,27 @@
 package com.finsight.backend.adapter;
 
-import com.finsight.backend.dto.response.DepositDetailDto;
-import com.finsight.backend.dto.response.EtfDetailDetailDto;
-import com.finsight.backend.dto.response.FundDetailDetailDto;
-import com.finsight.backend.dto.response.ProductDetailDto;
-import com.finsight.backend.enumerate.ProductType;
 import com.finsight.backend.vo.Deposit;
 import com.finsight.backend.vo.Etf;
 import com.finsight.backend.vo.Fund;
 import com.finsight.backend.vo.Product;
+import org.springframework.stereotype.Component;
 
 import java.util.Map;
 
+@Component
 public class ProductAdapter {
 
-    private static final Map<String, Class<? extends Product>> productTypeMap = Map.of(
-            "deposit", Deposit.class,
-            "fund", Fund.class,
-            "etf", Etf.class
-    );
+    private final Map<String, Class<? extends Product>> productTypeMap;
 
-    public static Class<? extends Product> productType(String category){
+    public ProductAdapter() {
+        this.productTypeMap = Map.of(
+                "deposit", Deposit.class,
+                "fund", Fund.class,
+                "etf", Etf.class
+        );
+    }
+
+    public Class<? extends Product> productType(String category){
         return productTypeMap.get(category.toLowerCase());
     }
 }
