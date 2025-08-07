f565f61d987c0206e497128aeeed7a247183a7e0
sangwuny <144337247+sangwuny@users.noreply.github.com>
2025-08-07 15:17:57 +0900

    :bug: fix : 회원가입 탈퇴 UI 수정

--- vite.config.js
+++ vite.config.js
@@ -32,9 +32,9 @@ export default defineConfig({
         target: 'http://localhost:8080',
         changeOrigin: true,
         secure: false,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      },
-    },
-    host: true
+        rewrite: path => path.replace(/^\/api/, '')
+      }
+    }
+    // host: true
   }
 });
