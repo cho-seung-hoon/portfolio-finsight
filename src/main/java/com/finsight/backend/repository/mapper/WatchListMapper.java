642f24a5f69e546d46201103bf15e2cc984a1f79
JiYoon Yang <yoon2fy@gmail.com>
2025-08-16 10:59:00 +0900

    🐛 fix : WatchList 등록/해제

--- src/main/java/com/finsight/backend/repository/mapper/WatchListMapper.java
+++ src/main/java/com/finsight/backend/repository/mapper/WatchListMapper.java
@@ -2,10 +2,15 @@ package com.finsight.backend.repository.mapper;
 
 import com.finsight.backend.dto.WatchListDTO;
 import org.apache.ibatis.annotations.Mapper;
-
-import java.util.List;
+import org.apache.ibatis.annotations.Param;
 
 @Mapper
 public interface WatchListMapper {
-    List<WatchListDTO> selectWatchListDepositByUserId(String userId);
+    // 찜한 상품 등록
+    int insertWatch(WatchListDTO watchListDTO);
+
+    // 찜한 상품 해제
+    int deleteWatch(@Param("userId") String userId,
+                    @Param("productCode") String productCode,
+                    @Param("productCategory") String productCategory);
 }
